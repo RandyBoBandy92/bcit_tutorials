dd9f87d96694f2446dd62796cbe37307021dd7aa
Randy Gulak <randy@randygulak.com>
2024-11-08 11:29:34 -0800

    added code pencil as html, css, js so i can edit it more easily.

---
+++ playground/codePencil/templates/codePencilSite/scriptCodePencil.js
@@ -0,0 +1,29 @@
+const words = ["Sketch", "Edit", "Create", "Practice"];
+let currentWordIndex = 0;
+let currentLetterIndex = 0;
+const dynamicWordElement = document.querySelector('.dynamic-word');
+
+function typeWord() {
+  if (currentLetterIndex < words[currentWordIndex].length) {
+    dynamicWordElement.textContent += words[currentWordIndex].charAt(currentLetterIndex);
+    currentLetterIndex++;
+    setTimeout(typeWord, 150);
+  } else {
+    setTimeout(eraseWord, 2000); // Keep word displayed for 750ms
+  }
+}
+
+function eraseWord() {
+  if (currentLetterIndex > 0) {
+    dynamicWordElement.textContent = dynamicWordElement.textContent.slice(0, -1);
+    currentLetterIndex--;
+    setTimeout(eraseWord, 200);
+  } else {
+    currentWordIndex = (currentWordIndex + 1) % words.length;
+    setTimeout(typeWord, 200);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  typeWord();
+});
